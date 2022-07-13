b2886bf81f8c7e71a3515fbe7025d04d4902d0b8
zuka <zuka@DESKTOP-01OQQMQ>
2022-07-13 21:39:09 +0400

    Add project files.

---
+++ CountVariants/Program.cs
@@ -0,0 +1,17 @@
+﻿static int CountVariants(int stairCount)
+{
+    if (stairCount < 2)
+    {
+        return stairCount;
+    }
+
+    int[] ans = new int[stairCount];
+    ans[0] = 1;
+    ans[1] = 2;
+
+    for (int i = 2; i < stairCount; i++)
+    {
+        ans[i] = ans[i - 1] + ans[i - 2];
+    }
+    return ans[stairCount - 1];
+}
